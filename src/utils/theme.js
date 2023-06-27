5698ca67028951448743cabb0391ce8144a98efc
Abhiram <a4abhiram125@gmail.com>
2023-06-28 01:23:31 +0530

    updated theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,10 +3,14 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFFFFF",// Customize the primary color
-      light:"#FAFBFF",
-      contrastText: "#333",
-    }
+      main: "#FFFFFF", // Customize the primary color
+      light: "#FAFBFF",
+      contrastText: "#292D32",
+    },
+    secondary: {
+      main: "#5932EA",
+      contrastText: "#ACACAC",
+    },
   },
 });
 
